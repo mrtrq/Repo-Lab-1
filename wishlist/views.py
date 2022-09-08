d9cb08870f0787977bbc94bed1356ac6216f8849
mtrq <muhammad.tarreq@ui.ac.id>
2022-09-08 15:45:58 +0700

    Push Lab 1

---
+++ wishlist/views.py
@@ -0,0 +1,14 @@
+from multiprocessing import context
+import re
+from django.shortcuts import render
+from wishlist.models import BarangWishlist
+
+# Create your views here.
+def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Tarreq Maulana'
+    }
+    return render(request,'wishlist.html', context)
+    
