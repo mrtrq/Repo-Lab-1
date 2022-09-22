99dd7b6ef04b2c0e2261f4d1169f56b0bfa54506
mtrq <muhammad.tarreq@ui.ac.id>
2022-09-22 15:12:22 +0700

    form, auth, and cookies

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,9 @@ from wishlist.views import getXML
 from wishlist.views import getJSON
 from wishlist.views import getIdBasedJSON
 from wishlist.views import getIdBasedXML
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
 
 app_name = 'wishlist'
 
@@ -13,4 +16,8 @@ urlpatterns = [
     path('json/', getJSON, name='getJSON'),
     path('xml/<int:id>', getIdBasedXML, name='getIdBasedXML'),
     path('json/<int:id>', getIdBasedJSON, name='getIdBasedJSON'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+
 ]
